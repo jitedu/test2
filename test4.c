80cadec9483e8bc3f565a5f111efc459c77361c6
Dqiyue <2470782476@qq.com>
2014-05-04 22:34:04 +0800

    duhui

---
+++ test4.c
@@ -0,0 +1,42 @@
+#include "student_info.h"
+#include <stdio.h>
+
+
+int main()
+{
+
+	int num[40];
+	int i;
+	int k;
+	int j;
+	int l=0;
+	int flag=0;
+	
+	for (i = 0; i < sizeof(info)/sizeof(struct student); ++i) {
+		k=rand()%40;
+	if(i==0) { num[0]=k;l++;}
+	else{
+		while(flag==0)
+		{
+
+			for(j=0;j<l;j++)
+			{
+				if(num[j]==k)
+				{
+					k=rand()%40;
+					break;
+				}
+			}
+			if(j==l)flag=1;
+		}
+		flag=0;
+		num[l]=k;
+		l++;
+
+	}
+		printf("%d - %s\n", info[i].id, info[k].name);
+		if(l%4==0)
+		  printf("\n\n\n");
+	}
+	return 0;
+}
